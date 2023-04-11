fc1d23161a1887a0e3441983cdba5ad05243b4df
privetandy <90655861+privetandy@users.noreply.github.com>
2023-04-11 03:43:32 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const startBtn = document.querySelector('.product')
-const startBtn2= document.querySelector('.product-business')
+const TWORK= document.querySelector('.TWORK')
 const screens = document.querySelectorAll('.screen')
+const back = document.querySelector('.back')
+const back2 = document.querySelector('.back2')
+const backmain = document.querySelector('.backmain')
+const open = document.getElementById('open');
 
+const modal_cont = document.getElementById('modal_cont');
+const modal = document.getElementById('modal')
 
 
 startBtn.addEventListener('click', (event) => {
@@ -12,14 +18,41 @@ screens[0].classList.add('up')
 
 })
 
-startBtn2.addEventListener('click', (event) => {
-    event.preventDefault()
-        if (event.target.classList.contains('product-business-btn')){
+TWORK.addEventListener('click', (event) => {
+   
 screens[1].classList.add('up') 
-}
+
 
 })
 
 
 
 
+back.addEventListener('click',(event) => {
+    screens[0].classList.remove('up')
+})
+
+back2.addEventListener('click',(event) => {
+    screens[1].classList.remove('up')
+})
+
+backmain.addEventListener('click',(event) => {
+    screens[0].classList.remove('up')
+    screens[1].classList.remove('up')
+})
+
+
+open.addEventListener('click', (event)=> {
+    modal_cont.classList.add('show');
+    modal.innerHTML = '<div><iframe width="860" height="615" src="https://www.youtube.com/embed/TR-cakkJ-4k?autoplay=1&controls=0"  frameborder="0"  allowfullscreen></iframe> </div>';
+
+});
+
+const close = document.getElementById('close'); 
+
+close.addEventListener('click', (event)=> {
+    modal_cont.classList.remove('show');
+    modal.innerHTML = ' ';
+    
+    
+});
